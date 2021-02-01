55f8893a27d3830f8082a3fd05b135636271a1fb
Sutolix <tg884ght@gmail.com>
2021-02-01 20:51:32 -0300

    recebimento das informações na página de detalhes

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { Text, View, ScrollView, Image, Linking, Pressable } from 'react-native';
+import { Text, View, ScrollView, Image, Linking, Pressable, FlatList } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native'
 
 import styles from './styles.js'
 
 import api from '../../services/api';
-import { FlatList } from 'react-native-gesture-handler';
 
 export default function Details() {
   const navigation = useNavigation()
@@ -14,31 +13,41 @@ export default function Details() {
   const dealID = route.params.dealID
 
   const [gameInfo, setGameInfo] = useState({
+    cheaperStores : [],
+    metacriticScore: '',
     name: '',
-    price: '',
-    cheaperStores : {
-      dealID: '',
-      salePrice: '',
-      retailPrice: ''
-    },
+    publisher: '',
+    salePrice: '',
+    steamRatingPercent: '',
+    steamRatingText: '',
+    thumb: '',
   })
   const [isLoading, setIsLoading] = useState(true)
+  const [hasCheaperStores, setHasCheaperStores] = useState(false)
 
   async function LoadGameDetails() {
 
     try {
-      await api.get(`deals?id=X8sebHhbc1Ga0dTkgg59WgyM506af9oNZZJLU9uSrX8%3D`).then(response => {
+      await api.get(`deals?id=${dealID}`).then(response => {
         let infos = response.data
+        
+        let cheaperStores = infos.cheaperStores
+        //return: dealID, retailPrice, salePrice and store ID
+
+        if (cheaperStores.length !== 0) {
+          setHasCheaperStores(true)
+        }
+
         setGameInfo({
+          cheaperStores,
+          metacriticScore: infos.gameInfo.metacriticScore,
           name: infos.gameInfo.name,
-          price: infos.gameInfo.salePrice,
-          cheaperStores: {
-            dealID: infos.cheaperStores.dealID,
-            salePrice: infos.cheaperStores.salePrice,
-            retailPrice: infos.cheaperStores.retailPrice,
-          }
+          publisher: infos.gameInfo.publisher,
+          salePrice: infos.gameInfo.salePrice,
+          steamRatingPercent: infos.gameInfo.steamRatingPercent,
+          steamRatingText: infos.gameInfo.steamRatingText,
+          thumb: infos.gameInfo.thumb,
         })
-        console.log(infos.cheaperStores)
         setIsLoading(false)
       })
 
@@ -59,6 +68,7 @@ export default function Details() {
   function openOnBroswer(dealID) {
     Linking.openURL(`https://www.cheapshark.com/redirect?dealID=${dealID}`)
   }
+  
 
   return (
     <View style={styles.container}>
@@ -66,11 +76,34 @@ export default function Details() {
         <Text style={styles.pageHeaderText}>Game Details</Text>
       </View>
       {isLoading ? <Text>Loading</Text> :
+      <View>
         <ScrollView>
-            <Text>{gameInfo.name}</Text>
+            <Text>Name: {gameInfo.name}</Text>
+            <Text>Publisher: {gameInfo.publisher}</Text>
+            <Text>Price: {gameInfo.salePrice}</Text>
+            <Text>Steam Rating: {gameInfo.steamRatingText}</Text>
         </ScrollView>
+        {hasCheaperStores ?
+        <FlatList
+          data={gameInfo.cheaperStores}
+          style={{height:500, marginTop:20}}
+          keyExtractor={item => item.id}
+          renderItem={({index}) => (
+            <View style={{marginBottom:10, backgroundColor:'green'}}>
+              <Text>dealID:</Text>
+              <Text>{gameInfo.cheaperStores[index].dealID}</Text>
+
+              <Text>retailPrice:</Text>
+              <Text>{gameInfo.cheaperStores[index].retailPrice}</Text>
+
+              <Text>salePrice:</Text>
+              <Text>{gameInfo.cheaperStores[index].salePrice}</Text>
+            </View>
+          )}
+        />
+        : <></>}
+        </View>
       }
     </View>
   )
 }
-View
